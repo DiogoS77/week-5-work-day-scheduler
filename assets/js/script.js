5314d6a3e1235be0d6d7f1faad1eb9f706eeb153
DiogoS77 <diogo.messi.18@hotmail.com>
2023-05-06 17:44:47 +0100

    added update times  lots js code

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,4 +3,22 @@ var options = {
   endHour: 5,
 };
 
-function updateTimeslots() {}
+function updateTimeslots() {
+  console.log("updateTimeslots");
+  var currentHour = dayjs().hour();
+
+  $(".time-block").each(function () {
+    var hour = $(this).attr("data-hour");
+    console.log(hour, currentHour);
+
+    $(this).find(".description").removeClass("past present future");
+
+    if (hour < currentHour) {
+      $(this).find(".description").addClass("past");
+    } else if (hour == currentHour) {
+      $(this).find(".description").addClass("present");
+    } else {
+      $(this).find(".description").addClass("future");
+    }
+  });
+}
